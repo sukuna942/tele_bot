268043e92215c215ffaa59b114d62523484b3b6e
sukuna942 <146425410+sukuna942@users.noreply.github.com>
2024-03-19 12:51:27 +0300

    config.py

---
+++ extensions/config.py
@@ -0,0 +1,7 @@
+TOKEN_BOT = r"6908909758:AAHkSm2QwoQD5ii73FPFSSLDJKGAtgSE-Lc"
+API_KEY = "6c5ccaaadd288c325930b3c4589d5a96f4cbb243604a9a5eee36badd4cd80c15"
+list_of_currencies = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
